50e7b8e06411c5622288d2d333d84ec8119cad5b
Esther <24371125+Estherns@users.noreply.github.com>
2020-01-15 20:06:04 +0300

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -1,27 +1,35 @@
 import time
 from datetime import datetime as dt 
 
-hosts_temp = 'hosts' 
-host_path = r'C:\Windows\System32\drivers\etc\hosts'
+host_file = 'hosts' 
+#Normally the host file is located on the path below, therefore when it comes to actual project implementation, change host_file to host_path
+#host_path = r'C:\Windows\System32\drivers\etc\hosts
 
+# initialize the redirect path
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com', 'facebook.com', 'www.twitter.com', 'twitter.com']
 
 while True:
+	# condition to check if current time is within the working hours and append the content by including the website address in the hosts file
 	if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16,):
-		print('working hours...')
-		with open(hosts_temp, 'rt') as file:
+		print('Lol! You want to chat now! Its Working time...')
+		with open(host_file, 'rt') as file:
 			content = file.read()
 			for website in website_list:
 				if website in content:
 					pass
 				else:
 					file.write(redirect+""+ website+ "\n")
+	# condition to check if current time is not within the working hours and append the content by deleting the website address in the hosts file
 	else:
-		with open(hosts_temp, 'rt') as file:
+		with open(host_file, 'rt') as file:
 			content = file.readlines()
 			for line in content:
-				print('Fun hours')
+				if not any(website in line for website in website_list):
+					file.write(line)
+				file.truncate()
+
+		print('Social time...')
 	time.sleep(5)
 
 
